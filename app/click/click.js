aa8c405809fd00aa1b57f800d4041eeec43d9e36
angvishvish <angvishvish@gmail.com>
2016-04-09 23:13:12 +0530

    modified all the files

--- app/click/click.js
+++ app/click/click.js
@@ -9,6 +9,21 @@ angular.module('myApp.click', ['ngRoute'])
   });
 }])
 
-.controller('ClickCtrl', [function() {
-
+.controller('ClickCtrl', ['$scope', function($scope) {
+  $scope.clickArray = [];
+  $scope.clickEvent = function (event) {
+    var clickObj = {
+      xCordinate: event.clientX,
+      yCordinate: event.clientY,
+      timestamp: new Date()
+    }
+    $scope.clickArray.push(clickObj);
+    $scope.showClick(clickObj);
+  }
+  $scope.showClick = function (clickObj) {
+    var clickHtml = '<span class="circle-span animated fadeIn" \
+                     style="top: ' + (clickObj.yCordinate - 15) + 'px; \
+                     left: ' + (clickObj.xCordinate - 15) + 'px"></span>';
+    $('.click-playground').append(clickHtml)
+  }
 }]);
